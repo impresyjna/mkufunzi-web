a24fda9d08a7faa280664fd57d1bd1a44cb8c671
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2015-11-23 00:05:02 +0100

    Title test and creation

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Strona domowa | Trener-Mkufunzi"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "O nas | Trener-Mkufunzi"
   end
 
 end
