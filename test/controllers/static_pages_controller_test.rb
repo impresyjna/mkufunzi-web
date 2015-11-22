b80b26424ee4d0bdc2e2c571101d7ce9bd5663c8
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2015-11-23 00:35:40 +0100

    New tests for title and test contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Trener-Mkufunzi"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Strona domowa | Trener-Mkufunzi"
+    assert_select "title", "Strona domowa | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "O nas | Trener-Mkufunzi"
+    assert_select "title", "O nas | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Kontakt | #{@base_title}"
   end
 
 end
