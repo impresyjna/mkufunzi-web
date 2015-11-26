e617a8de3cd319e18ab7c81aba468bbaa09deb0b
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2015-11-26 21:42:55 +0100

    Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109155634) do
+ActiveRecord::Schema.define(version: 20151126203430) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "login",      limit: 255
+    t.string   "email",      limit: 255
     t.string   "name",       limit: 255
     t.string   "surname",    limit: 255
-    t.integer  "age",        limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
