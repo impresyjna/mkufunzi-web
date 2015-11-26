07343b203b6c0a7d93db5205d9738c762a07d6a1
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2015-11-26 21:29:14 +0100

    Sign up page, user controller, tests for user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root             'static_pages#home'
   get 'contact' => 'static_pages#contact'
-  get 'about' => 'static_pages#about'
+  get 'about'   => 'static_pages#about'
+  get 'signup'  => 'users#new'
 end
