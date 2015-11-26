a60a4ddfba3d2de438cb9ad345315bd1d4b8c7d0
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2015-11-26 22:41:16 +0100

    User validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  validates :login, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+      format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :surname, presence: true, length: { maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126203430) do
+ActiveRecord::Schema.define(version: 20151126213714) do
 
   create_table "users", force: :cascade do |t|
     t.string   "login",      limit: 255
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20151126203430) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
