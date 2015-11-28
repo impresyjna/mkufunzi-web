aa38fd154a006125b10f66a9367be1b058e5e1d2
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2015-11-28 22:27:53 +0100

    Log in and log out user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'about'   => 'static_pages#about'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 end
