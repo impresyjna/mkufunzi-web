4b1fccbce28c35ee2741e7056ff5b43639e7ca37
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-01-09 11:30:08 +0100

    New serializer for measurement

--- app/serializers/measurement_serializer.rb
+++ app/serializers/measurement_serializer.rb
@@ -1,3 +1,3 @@
 class MeasurementSerializer < ActiveModel::Serializer
-  attributes :id, :value, :created_at, :measure_type_id, :card_id
+  attributes :id, :value, :created_at, :measure_type_id, :card_id, :second_value
 end
