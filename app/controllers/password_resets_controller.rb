5b6703e21b8c76bf76f32caf90828b2a176cf9ff
romeeek <romekkaczorowski@gmail.com>
2016-01-24 21:18:41 +0100

    change flash to polish

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,10 +11,10 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = "Na podany e-mail została wyslana wiadomosc z linkiem resetującym hasło."
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = "Nie poprawy e-mail."
       render 'new'
     end
   end
@@ -28,7 +28,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)
       log_in @user
-      flash[:success] = "Password has been reset."
+      flash[:success] = "Hasło zostało zmienione"
       redirect_to @user
     else
       render 'edit'
@@ -58,7 +58,7 @@ class PasswordResetsController < ApplicationController
     # Checks expiration of reset token.
     def check_expiration
       if @user.password_reset_expired?
-        flash[:danger] = "Password reset has expired."
+        flash[:danger] = "Link wygasł"
         redirect_to new_password_reset_url
       end
     end
