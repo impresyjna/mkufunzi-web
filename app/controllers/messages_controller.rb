5690da5cb88a550cb9ffd640864e6eeabe18501b
Asia <joanna.furmaniak@student.put.poznan.pl>
2016-01-27 11:10:31 +0100

    Messages API

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,10 +31,33 @@ class MessagesController < ApplicationController
     end
   end
 
+  def my_messages_index_mobile
+    if !params[:id].nil?
+      @user = User.find(params[:id])
+      @messages = Message.where("user_send_id = ?  OR user_receive_id = ?", @user.id, @user.id)
+      render json: {status: "success", messages: @messages }
+    else
+      render json: {status: "failure"}
+    end
+  end
+
+  def create_message_mobile
+    @message = Message.new(message_mobile)
+    if @message.save
+      render json: {status: "success"}
+    else
+      render json: {status: "failure"}
+    end
+  end
+
   private
 
     def message_params
   		params.require(:message).permit(:content, :user_send_id, :user_receive_id)
-  	end
+    end
+
+    def message_mobile
+      params.permit(:content, :user_send_id, :user_receive_id)
+    end
 
 end
