575bd7b587e76186b9cdb940b87a49aef6ec3f9c
Asia <joanna.furmaniak@student.put.poznan.pl>
2016-02-01 20:52:19 +0100

    For debug emergency operation

--- config/routes.rb
+++ config/routes.rb
@@ -76,9 +76,9 @@ Rails.application.routes.draw do
   get 'training' => 'trainings#show'
   get 'training_details' => 'trainings#show_details'
 
-  post 'create_active_excercise_mobile' => 'active_excercises#create_active_excercise_mobile'
+  get 'create_active_excercise_mobile' => 'active_excercises#create_active_excercise_mobile'
   post 'update_active_excercise_mobile' => 'active_excercises#update_active_excercise_mobile'
-  get 'create_training_mobile' => 'trainings#create_training_mobile'
+  post 'create_training_mobile' => 'trainings#create_training_mobile'
   post 'update_training_mobile' => 'trainings#update_training_mobile'
   post 'end_active_excercise_mobile' => 'active_excercises#end_active_excercise_mobile'
   get 'my_messages_index_mobile' => 'messages#my_messages_index_mobile'
